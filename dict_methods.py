42cb9cc666b7c51d0c71b5267de08e3d0ec51846
vafanaso <afanava2@cvut.cz>
2025-12-06 13:23:13 +0100

    added dict_methods

--- dict_methods.py
+++ dict_methods.py
@@ -58,12 +58,11 @@ def send_to_store(cart:dict, aisle_mapping:dict):
     """
 
     return_dict:dict = {}
-    right:list = []
     for key in aisle_mapping.keys():
         if key in cart.keys():
             aisle_mapping[key].insert(0, cart[key])
             return_dict.setdefault(key, aisle_mapping[key])
-    return return_dict
+    return dict(sorted(return_dict.items(), reverse=True))
 
 
 def update_store_inventory(fulfillment_cart, store_inventory):
@@ -73,27 +72,15 @@ def update_store_inventory(fulfillment_cart, store_inventory):
     :param store_inventory: dict - store available inventory
     :return: dict - store_inventory updated.
     """
+    right:list = []
+    for key in fulfillment_cart.keys():
+        right = store_inventory[key]
+        if (right[0] - fulfillment_cart[key][0]) <=0:
+            right[0] =  'Out of Stock'
+        else:
+            right[0] = right[0] - fulfillment_cart[key][0]
+        store_inventory.setdefault(key, right)
+    return store_inventory
 
-    pass
-
-
-
-# print(add_item({'Banana': 3, 'Apple': 2, 'Orange': 1},
-#               ('Apple', 'Apple', 'Orange', 'Apple', 'Banana')))
-# print("=============================")
-# print(add_item({'Banana': 3, 'Apple': 2, 'Orange': 1},
-#               ['Banana', 'Orange', 'Blueberries', 'Banana'])
-# )
-# print("=============================")
-# print(update_recipes(
-#     {'Banana Bread' : {'Banana': 1, 'Apple': 1, 'Walnuts': 1, 'Flour': 1, 'Eggs': 2, 'Butter': 1},
-#     'Raspberry Pie' : {'Raspberry': 1, 'Orange': 1, 'Pie Crust': 1, 'Cream Custard': 1},
-#     'Pasta Primavera': {'Eggs': 1, 'Carrots': 1, 'Spinach': 2, 'Tomatoes': 3, 'Parmesan': 2, 'Milk': 1, 'Onion': 1}},
-#     [('Raspberry Pie', {'Raspberry': 3, 'Orange': 1, 'Pie Crust': 1, 'Cream Custard': 1, 'Whipped Cream': 2}),
-#     ('Pasta Primavera', {'Eggs': 1, 'Mixed Veggies': 2, 'Parmesan': 2, 'Milk': 1, 'Spinach': 1, 'Bread Crumbs': 1}),
-#     ('Blueberry Crumble', {'Blueberries': 2, 'Whipped Creme': 2, 'Granola Topping': 2, 'Yogurt': 3})]
-#     ))
-# print (sort_entries({'Banana': 3, 'Apple': 2, 'Orange': 1}))
-# print(read_notes(['Blueberries', 'Pear', 'Orange', 'Banana', 'Apple']))
 print(send_to_store({'Banana': 3, 'Apple': 2, 'Orange': 1, 'Milk': 2},
                   {'Banana': ['Aisle 5', False], 'Apple': ['Aisle 4', False], 'Orange': ['Aisle 4', False], 'Milk': ['Aisle 2', True]}))
