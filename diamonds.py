5b98c432b5e7ac3549656661fd16eb9ff3410bfb
vafanaso <afanava2@cvut.cz>
2025-12-07 14:42:31 +0100

    Added diamonds

--- diamonds.py
+++ diamonds.py
@@ -25,14 +25,15 @@ def row_maker(current_letter, input_latter) -> tuple:
     index_curentletter = ALPHA_UP.index(current_letter)
     index_inputletter = ALPHA_UP.index(input_latter)
     letters_delta = index_inputletter -index_curentletter
-    number_of_row = index_curentletter + 1
     row:list = []
     for index in range(index_inputletter * 2 + 1):
         row.append('.')
     row[letters_delta] = current_letter
     row[-1 *(letters_delta +1)]  = current_letter
     my_row = ''.join(row)
-    return (number_of_row, my_row)
+    return (my_row)
+
+
 
 
 def rows(letter:str) -> None:
@@ -41,17 +42,15 @@ def rows(letter:str) -> None:
 
     for index in range(index_inputletter + 1):
         list_of_rows.append(row_maker(ALPHA_UP[index], letter))
-    new_row_number = list_of_rows[-1][0] + 1
     for index in range(index_inputletter-1, -1, -1):
-        temp_row = (new_row_number,list_of_rows[index][1])
+        temp_row = list_of_rows[index]
         list_of_rows.append(temp_row)
-        new_row_number += 1
 
-
-    for item in list_of_rows:
-        print(item[1])
+     for item in list_of_rows:
+        print(item)
 
 
 
+rows('N')
 
 
